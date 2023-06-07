0dd1e239634e9af99767371241e69fcae185e31f
kajela <titoyut56@gmail.com>
2023-06-07 04:42:20 -0400

    add the initial files

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,10 +1,21 @@
 import React from "react";
+// import Article from "./Article";
+
 
 function ArticleList({posts}){
+    const articles = posts.map((post) =>{
+        <ArticleList
+        key={post.id}
+        title ={post.title}
+        date ={post.date}
+        preview ={post.preview}
+        minutes ={post.minutes}
+        />
+    });
     return(
         <main>
-            <article key={posts.id}>{posts}</article>
+            {articles}
         </main>
     )
 }
-ex
+export default ArticleList;
